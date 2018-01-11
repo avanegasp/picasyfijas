4fc3674a85a942a46a12c2aebf332bc43114b2db
avanegasp <arco527@gmail.com>
2018-01-11 12:12:04 -0500

    Fixing html and css

--- app/app.js
+++ app/app.js
@@ -2,28 +2,31 @@
 
 
 
-$('body').keyup(function(e){
-  event.preventDefault();
+$(document).ready(function(){
   var number = 999 + Math.floor(Math.random() * 9000);
   console.log(number)
-  var value = $('input').val()
-  if(e.keyCode==13){
-    value
+  $('input').keyup(function(e){
+    event.preventDefault();
+    var value = $('input').val()
+    console.log(value)
+    if(e.keyCode==13){
+      value
+      $('.table').append(function(){
+             return "<td>"  + value +"";
+       });
+             $(this).val('');
+    }
+     if (value.length >= 4){
+      alert("tiene más de 4 caracteres")
+    } if (value === number){
+      console.log(value)
+      console.log('Son los mismos números')
+    }
+    // if(value === number){
+    //   console.log("números iguales")
+    // }else{
+    //   alert("no son iguales")
+    // }
 
-    // console.log(value)
-
-    $('.table').append(function(){
-           return "<td>"  + value +"";
-     });
-           $(this).val('');
-  }
-  if (value.length >= 4){
-    alert("tiene más de 4 caracteres")
-  }
-  // if(value === number){
-  //   console.log("números iguales")
-  // }else{
-  //   alert("no son iguales")
-  // }
-
+  })
 })
