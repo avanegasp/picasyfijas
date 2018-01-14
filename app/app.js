6c2637918cb8f3c2ce601f41b9ff016bcc4ef03d
avanegasp <arco527@gmail.com>
2018-01-13 19:38:23 -0500

    Loop for with bugs

--- app/app.js
+++ app/app.js
@@ -1,32 +1,71 @@
+//
+//
+//
+//
+// $(document).ready(function(){
+//   var number = 999 + Math.floor(Math.random() * 9000);
+//   console.log(number)
+//
+//
+//     $('input').keyup(function(e){
+//
+//     var value = $('input').val()
+//     console.log(value)
+//
+//     var checkNumber = isNaN(value)
+//     console.log("no es un número")
+//
+//     var differentLength = value >= 4
+//
+//     var checkRepeat = value.split("")
+//     // console.log("se repiten números")
+//
+//     if(e.keyCode==13){
+//       value
+//       $('.table').append(function(){
+//              return "<td>"  + value +"";
+//        });
+//              $(this).val('');
+//     }
+//
+//    if (value === number){
+//     console.log(value)
+//     console.log('Son los mismos números')
+//     }if (value.length >= 4){
+//       alert("tiene más de 4 caracteres")
+//     }
+//   })
+// })
 
 
+$('document').ready(function(){
 
-
-$(document).ready(function(){
-  var number = 999 + Math.floor(Math.random() * 9000);
-  console.log(number)
   $('input').keyup(function(e){
-    event.preventDefault();
     var value = $('input').val()
     console.log(value)
-    if(e.keyCode==13){
-      value
-      $('.table').append(function(){
-             return "<td>"  + value +"";
-       });
-             $(this).val('');
-    }
-     if (value.length >= 4){
-      alert("tiene más de 4 caracteres")
-    } if (value === number){
-      console.log(value)
-      console.log('Son los mismos números')
-    }
-    // if(value === number){
-    //   console.log("números iguales")
-    // }else{
-    //   alert("no son iguales")
-    // }
 
+
+    if (e.keyCode === 13){
+      value
+    }
+
+    repeatNumber = value.split('')
+    console.log(repeatNumber)
+
+
+    var result = []
+
+    for(var i=0; i < repeatNumber.length; i++){
+      repeatNumber[i] = repeatNumber.push(repeatNum
+    }
+    result
+    console.log(result)
   })
+
 })
+
+
+//
+// var num = 123456;
+// var digits = num.toString().split('');
+// console.log(digits);
